003d387bc240ebabe199deb7212588f942733fb7
Scott Lowe <scott@entercastle.com>
2015-05-19 10:50:30 -0400

    feat(JobQueueProvider): add constructor and  method

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-module.exports = {};
+var Firebase = require('firebase');
+var Worker = require('firebase-queue');
+
+var JobQueueProvider = function (url) {
+  if (!url) { throw new Error('Firebase url required'); }
+  var _ref = new Firebase(url);
+
+  this.createWorker = function (processJob) {
+    if (!processJob) { throw new Error('Process job function required'); }
+    var options = { numWorkers: 3 };
+    var worker = new Worker(_ref.child('_queue'), options, function (data, progress, resolve, reject) {
+      processJob(data, function () {
+        _ref.child('queue').child('completedTasks').push(data, function (err) {
+          if (err) { throw err; }
+          else { resolve(); }
+        });
+      });
+    });
+  }
+}
+
+module.exports = JobQueueProvider;
--- test/unit/index.js
+++ test/unit/index.js
@@ -6,10 +6,23 @@ var expect = chai.expect;
 chai.use(sinonChai);
 chai.use(chaiAsPromised);
 
+var JobQueueProvider = require('../../src/index.js');
+var firebaseUrl = 'https://test.firebaseio.com/';
+
 describe('unit tests::', function () {
+  var provider;
   it('should respect logic', function () {
     expect(true).to.be.true;
     expect(true).not.to.be.false;
   });
-  //TODO: add unit tests here
+  it('should be constructable', function () {
+    expect(function () {
+      provider = new JobQueueProvider(firebaseUrl);
+    }).not.to.throw();
+  });
+  it('should require a firebase url', function () {
+    expect(function () {
+      provider = new JobQueueProvider();
+    }).to.throw('Firebase url required');
+  });
 });
