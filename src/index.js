ba2cef7e8401b0765b39d9f959970545e959d5af
Tim Crowe <tim.crowe@entercastle.com>
2016-05-25 18:39:15 -0700

    feat(index): support fast job queue type

--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ var Worker = require('firebase-queue');
  * A bridge between le-job-queue-service and Firebase
  * @class JobQueueProvider
  * @param {string} ref the firebase root reference
+ * @param {string} type the type of queue you want to create, supported types are 'default' and 'fast', leaving the field undefined is the same as 'default'
  * @returns {provider}
  */
-var JobQueueProvider = function (ref) {
+var JobQueueProvider = function (ref, type) {
   if (!ref) { throw new Error('Firebase reference required'); }
+  if (type !== 'fast' && type !== 'default' && type !== undefined) {
+    throw new Error('Invalid value for the type param, value: ' + type);
+  }
   var _ref = ref;
   /**
    * Creates a worker to process jobs
@@ -19,9 +23,15 @@ var JobQueueProvider = function (ref) {
   this.createWorker = function (processJob) {
     if (!processJob) { throw new Error('Process job function required'); }
     var options = { numWorkers: 1, specId: 'noTimeout' };
-    var worker = new Worker(_ref.child('_queue'), options, function (data, progress, resolve, reject) {
+    var queueLocation;
+    if(type === 'fast') {
+      queueLocation = 'fastQueue';
+    } else {
+      queueLocation = 'queue'
+    }
+    var worker = new Worker(_ref.child('_' + queueLocation), options, function (data, progress, resolve, reject) {
       processJob(data, function () {
-        _ref.child('queue').child('completedTasks').push(data, function (err) {
+        _ref.child(queueLocation).child('completedTasks').push(data, function (err) {
           if (err) { throw err; }
           else { resolve(); }
         });
